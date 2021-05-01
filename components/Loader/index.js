e9ca3d5548ec32256df34a8d3c590c18868556d7
rafAmorim8 <rafael_amorim30@hotmail.com>
2021-05-01 00:17:10 -0700

    Creates loader animation

---
+++ components/Loader/index.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import style from './loader.module.scss';
+
+import Lottie from 'react-lottie';
+import animationData from '../../public/loader.json';
+
+export default function Loader() {
+  const [animationState, setAnimationState] = useState({
+    isStopped: false, isPaused: false
+  });
+
+  const defaultOoptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    redererSettings: {
+      preserveAspectRation: 'xMidYMid slice'
+    }
+  }
+
+  return (
+    <div className={style.loaderContainer}>
+      <Lottie
+        options={defaultOoptions}
+        height={150}
+        width={150}
+        isStopped={animationState.isStopped}
+        isPause={animationState.isPaused}
+      />
+    </div>
+  );
+}
