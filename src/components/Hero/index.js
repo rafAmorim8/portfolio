b929999532c197a502c215a04fa7047b7376ca4d
rafAmorim8 <rafael_amorim30@hotmail.com>
2021-05-16 21:35:33 -0700

    Creates hero section layout

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,15 @@
 import styles from './hero.module.scss';
 
-export function Hero(){
+export function Hero() {
     return (
         <div className={styles.heroContainer}>
-            <p className="head-hello">Hello, my name is</p>
-            <p className="head-name">Rafael Amorim</p>
-            <p className="head-description">
-          I am a Computer Science student that wants to become a software
-          developer.
+            <p className={styles.heroHello}>Hello, my name is Rafael</p>
+            <p className={styles.heroInfo}>I build stuff for the web.</p>
+            <p className={styles.heroDescription}>
+                I am a recent graduate developer who loves to work with full stack applications.
             </p>
-            <button className="btn"><a href="#portfolio">See my Work</a>
-        </button>
+            <button className={styles.heroButton}><a href="#portfolio">see my work</a>
+            </button>
         </div>
-    )        
+    )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;700&family=Spectral:wght@300;400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
