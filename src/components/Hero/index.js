a5988acc31c1b236808650aa26794d3c2bdef206
rafAmorim8 <rafael_amorim30@hotmail.com>
2021-05-17 21:00:02 -0700

    Adds animation to hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,43 @@
+import { useRef, useEffect } from 'react';
 import styles from './hero.module.scss';
+import { TweenMax, TimelineLite, Power3 } from 'gsap';
 
 export function Hero() {
+    let content = useRef(null);
+
+    let tl = new TimelineLite({ delay: 0.5 });
+
+    useEffect(() => {
+        //Content vars
+        let headlineFirst = content.children[0];
+        let headlineSecond = headlineFirst.nextSibling;
+        let headlineThird = headlineSecond.nextSibling;
+        let heroButton = headlineThird.nextSibling;
+
+        TweenMax.to(content, 0, { css: { visibility: 'visible' } });
+
+        //Content animation
+        tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children], 1, {
+            y: 44,
+            ease: Power3.easeOut,
+            delay: .8
+        }, .15)
+            .from(heroButton, 1, { y: 25, opacity: 0, ease: Power3.easeOut }, 1.5)
+    }, [tl])
+
     return (
-        <div className={styles.heroContainer}>
-            <p className={styles.heroHello}>Hello, my name is Rafael</p>
-            <p className={styles.heroInfo}>I build stuff for the web.</p>
-            <p className={styles.heroDescription}>
-                I am a recent graduate developer who loves to work with full stack applications.
+        <div className={styles.heroContainer} ref={el => content = el}>
+            <div className={styles.heroHello}>
+                <p className={styles.heroHelloInner}>Hello, my name is Rafael</p>
+            </div>
+            <div className={styles.heroInfo}>
+                <p className={styles.heroInfoInner}>I build stuff for the web.</p>
+            </div>
+            <div className={styles.heroDescription}>
+                <p className={styles.heroDescriptionInner}>
+                    I am a recent graduate developer who loves to work with full stack applications.
             </p>
+            </div>
             <button className={styles.heroButton}><a href="#portfolio">see my work</a>
             </button>
         </div>
