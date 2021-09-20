55401a08594aa1e56562fa7b7d73dc969e77d9ec
rafAmorim8 <rafael_amorim30@hotmail.com>
2021-09-20 14:26:31 -0700

    Redesings the project cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,27 +1,31 @@
+import Image from 'next/image';
+
+
 import styles from './projectCard.module.scss';
 
-// import projOneImage from '../../Images/projectOneImage.jpg';
-// import projTwoImage from '../../Images/projectTwoImage.jpg';
-// import projThreeImage from '../../Images/projectThreeImage.jpg';
+export function ProjectCard({ id, name, imageName, description, technologies, url, gitRepo }) {
 
-export function ProjectCard({ id, name, description, technologies, url, gitRepo }) {
   return (
     <div
       className={styles.projectCardContainer}
-      style={{
-        backgroundImage: `url('../../Images/projectOneImage.jpg')`,
-      }}
     >
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p className={styles.technologies}>{technologies.map(tech => {
-        return (
-          <span>{tech}</span>
-        )
-      })}</p>
-      <div className={styles.links}>
-        <a href={`${gitRepo}`}>Git Repo</a>
-        <a href={`${url}`}>Visit Site</a>
+      <div className={styles.projectInfo}>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <ul className={styles.technologies}>{technologies.map(tech => {
+          return (
+            <li>{tech}</li>
+          )
+        })}</ul>
+        <div className={styles.links}>
+          <a href={`${gitRepo}`}><img className={styles.redirectLink} src="/githubLogo.svg" alt="See code in github" height={30} width={30} /></a>
+          <a href={`${url}`}><img className={styles.redirectLink} src="/externalLinkIcon.svg" alt="Visit website" /></a>
+        </div>
+      </div>
+      <div className={styles.projectImageWrapper}>
+        <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+          <Image className={styles.projectImage} src="/projectOneImage.jpg" alt="Project 1" layout="fill" object-fit="cover" />
+        </a>
       </div>
     </div >
   )
