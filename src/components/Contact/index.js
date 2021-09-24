c2e9f82db5c2428516b90c7874d68aab53e48d41
rafAmorim8 <rafael_amorim30@hotmail.com>
2021-09-24 16:06:05 -0700

    Adds animation to contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,30 @@
+import { useEffect, useRef } from 'react';
+
+import gsap, { Power3 } from 'gsap';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
 import styles from './contact.module.scss';
 
 export function Contact() {
+  const ref = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    gsap.from(ref.current, {
+      scrollTrigger: {
+        trigger: ref.current,
+        start: "30% bottom",
+      },
+      y: 30,
+      duration: 2,
+      opacity: 0,
+      ease: Power3.easeOut
+    });
+  }, []);
+
   return (
-    <section className={styles.contactContainer} id="contact">
+    <section className={styles.contactContainer} id="contact" ref={ref}>
       <h1>Contact Me</h1>
       <p>I am currently looking for a new job opportunity. Feel free to contact me and I will answer you as fast as I can.</p>
       <a className={styles.contactButton} href="http://www.google.com">Email me</a>
