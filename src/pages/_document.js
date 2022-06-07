1c474715daf76a3214f59a50b45cf819a3626ade
rafAmorim8 <rafael_amorim30@hotmail.com>
2022-06-07 10:36:24 -0700

    Feat: benchmark tool added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script type="text/javascript" src="/js/newrelic.js" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;700&family=Spectral:wght@300;400;700&display=swap" rel="stylesheet" />
         </Head>
